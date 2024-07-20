68fa780299165be9f6dac994667cb4bbe0153b4e
Joshua Inyang <joshuainyang255@gmail.com>
2024-07-20 15:03:40 +0100

    init commit: configurations and app structure

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-full overflow-auto">
+      <div className="mx-auto max-w-screen-xl h-full ">{children}</div>
+
+      <div className=" "></div>
+    </main>
+  );
+};
+
+export default LandingLayout;
